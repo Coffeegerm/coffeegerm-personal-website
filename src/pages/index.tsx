96b08ea75bed2d5f878a88b38d55a1862af31a4f
David Yarzebinski <dyarzebinski97@gmail.com>
2022-04-18 18:02:02 -0400

    add margin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,42 +3,46 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        flex: 1,
-        justifyContent: "space-around",
-        marginTop: "7rem",
-      }}
-    >
-      <section
+    <div>
+      <main
         style={{
           display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
+          flexDirection: "row",
+          flex: 1,
+          justifyContent: "space-around",
+          margin: "7rem 1rem 0 1rem",
+          flexWrap: "wrap",
         }}
       >
-        <h1 className="main-heading">
-          Hello. I&apos;m David. A software engineer.
-        </h1>
-        <h2>
-          &quot;The quieter you become, the more you can hear&quot; - Ram Dass
-        </h2>
-        <p>
-          I&apos;m also a lover of good coffee, tattoos, metal music, and books.
-        </p>
-      </section>
+        <section
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <h1 className="main-heading">
+            Hello. I&apos;m David. A software engineer.
+          </h1>
+          <h2>
+            &quot;The quieter you become, the more you can hear&quot; - Ram Dass
+          </h2>
+          <p>
+            I&apos;m also a lover of good coffee, tattoos, metal music, and
+            books.
+          </p>
+        </section>
 
-      <section>
-        <Image
-          src="/assets/IMG_1631.png"
-          height="500px"
-          width="400px"
-          alt="Me"
-        />
-      </section>
-    </main>
+        <section>
+          <Image
+            src="/assets/IMG_1631.png"
+            height="500px"
+            width="400px"
+            alt="Me"
+          />
+        </section>
+      </main>
+    </div>
   );
 };
 
